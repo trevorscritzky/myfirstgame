850c00871399d115f4e6a16e08bf409718bfa640
Trevor <tscritzky.de@mysummitps.org>
2016-10-14 21:03:57 +0000

    update

--- mygame/mygame.js
+++ mygame/mygame.js
@@ -17,12 +17,13 @@
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/star.png');
     game.load.image('diamond','assets/diamond.png');
+    game.load.image('firstaid','assets/firstaid.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
     function create() {
 //Background and stars
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0, 0, 'diamond');
+    game.add.sprite(0, 0, 'firstaid');
     game.add.sprite(0, 0, 'sky');
     platforms = game.add.group();
     platforms.enableBody = true;
@@ -34,18 +35,16 @@
     //Platform 1
     var ledge = platforms.create(0, 500, 'ground');
     ledge.scale.setTo(2, 1);
-    ledge.body.immovable = true
+    ledge.body.immovable = true;
     //Platform 1
     var ledge = platforms.create(200, 400, 'ground');
     ledge.scale.setTo(1, 1);
     ledge.body.immovable = true
-  
-   
 //Player plysics
     player = game.add.sprite(0, game.world.height - 111, 'dude');
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0;
-    player.body.gravity.y = 600;
+    player.body.gravity.y = 500;
     player.body.collideWorldBounds = true;
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
@@ -54,7 +53,7 @@
     stars.enableBody = true;
     for (var i = 0; i < 20; i++) {
 //Star physics
-        var star = stars.create(i * 40, 0, 'diamond');
+        var star = stars.create(i * 40, 0, 'firstaid');
         star.body.gravity.y = 300;
         star.body.bounce.y = 0.1 + Math.random() * 0.2;
         star.body.collideWorldBounds = true;
